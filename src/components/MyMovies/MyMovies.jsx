0f897cc094ac4e7fd971afe13752618a43435898
Therese Linden <theresemlinden@gmail.com>
2023-05-12 14:37:52 +0200

    added react icon instead of local image

--- src/components/MyMovies/MyMovies.jsx
+++ src/components/MyMovies/MyMovies.jsx
@@ -1,10 +1,10 @@
-import logo from '../../assets/view.png';
+import { BsEye } from 'react-icons/bs';
 import './myMovies.css';
 
 const MyMovies = () => {
   return (
-    <div className="seen-info">
-      <img src={logo} alt="Eye logo" />
+    <div className="my-movies">
+      <BsEye />
       <p>0</p>
     </div>
   );
