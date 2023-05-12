0d26efd928be81f2112efed32fa1632d3d171224
Therese Linden <theresemlinden@gmail.com>
2023-05-12 14:36:21 +0200

    removed variables

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,9 @@
+import './footer.css';
 const Footer = () => {
-  const copyright = String.fromCodePoint(0x00a9);
-  const text = 'Copyright MovieDB';
-  const year = '2023';
-
   return (
-    <p>
-      {copyright} {text} {year}
-    </p>
+    <footer>
+      <p>&copy; MovieDB Copyright 2023</p>
+    </footer>
   );
 };
 
