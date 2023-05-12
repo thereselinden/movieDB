00c6e6578123c6c30de7219948a6fcd8599b4994
Therese Linden <theresemlinden@gmail.com>
2023-05-12 14:05:27 +0200

    for future styling

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  darkMaroon: '#643843',
+  darkPink: '#99627A',
+  oldPink: '#C88EA7',
+  beige: '#E7CBCB',
+};
+
+const SIZES = [12, 16, 24, 32];
+
+export default { COLORS, SIZES };
