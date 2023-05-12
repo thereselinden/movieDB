1aa15103ba741184164f4af65e1013648168edd3
Therese Linden <theresemlinden@gmail.com>
2023-05-12 14:04:15 +0200

    render img

--- src/components/MyMovies/MyMovies.jsx
+++ src/components/MyMovies/MyMovies.jsx
@@ -1,7 +1,11 @@
+import logo from '../../assets/view.png';
+import './myMovies.css';
+
 const MyMovies = () => {
   return (
-    <div>
-      <h1>My Movies</h1>
+    <div className="seen-info">
+      <img src={logo} alt="Eye logo" />
+      <p>0</p>
     </div>
   );
 };
