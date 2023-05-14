879f4df1f30c69ce071ecd4860c646123c7382ff
Therese Linden <theresemlinden@gmail.com>
2023-05-14 19:00:31 +0200

    init fetch hook and print return variables

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -1,11 +1,20 @@
 import FilterDropDown from '../../components/FilterDropDown/FilterDropDown';
 import MovieCard from '../../components//MovieCard/MovieCard';
+import useFetch from '../../hooks/useFetch';
 
 const MovieList = () => {
+  const baseUrl = `${import.meta.env.VITE_API_URL}/popular?`;
+  const api_key = `api_key=${import.meta.env.VITE_API_KEY}`;
+  const url = baseUrl + api_key;
+
+  const { data: movies, isLoading, errorMessage } = useFetch(url);
+
   return (
     <div>
       <FilterDropDown />
-      <MovieCard />
+      {errorMessage && <p>Something went wrong</p>}
+      {isLoading && <p>Loading.....</p>}
+      {movies && <MovieCard movies={movies} />}
     </div>
   );
 };
