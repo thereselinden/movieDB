607f65a9a6cceacf37511cc77ce3dd2c55c3f3dd
Therese Linden <theresemlinden@gmail.com>
2023-05-17 15:45:29 +0200

    render basic movie info + corresponding css

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,23 @@
+import { AiTwotoneStar } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import MarkAsSeenBtn from '../MarkAsSeenBtn/MarkAsSeenBtn';
+import './moviecard.css';
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+  const imgUrl = 'https://image.tmdb.org/t/p/w500/';
   return (
-    <div>
-      <MarkAsSeenBtn />
-    </div>
+    <article>
+      <Link to={movie.id.toString()}>
+        <img src={imgUrl + movie.poster_path} alt={movie.title} />
+        <div>
+          <h3>{movie.title}</h3>
+          <p>
+            <AiTwotoneStar /> <span>{movie.vote_average}</span>
+          </p>
+          <MarkAsSeenBtn />
+        </div>
+      </Link>
+    </article>
   );
 };
 
