4d9073e9f89690bd02ed3f52e7a5fd607d0a4331
Therese Linden <theresemlinden@gmail.com>
2023-05-17 15:44:16 +0200

    render new fetch on category change

--- src/components/FilterDropDown/FilterDropDown.jsx
+++ src/components/FilterDropDown/FilterDropDown.jsx
@@ -1,6 +1,15 @@
-const FilterDropDown = () => {
+import { useEffect, useState } from 'react';
+import './filterDropDown.css';
+
+const FilterDropDown = ({ handleFilterChange }) => {
+  const [category, setCategory] = useState('popular');
+
+  useEffect(() => {
+    handleFilterChange(category);
+  }, [category, handleFilterChange]);
+
   return (
-    <select>
+    <select onChange={e => setCategory(e.target.value)}>
       <option value="popular">Popular</option>
       <option value="top_rated">Top Rated</option>
       <option value="upcoming">Upcoming</option>
