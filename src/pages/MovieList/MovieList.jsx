21d45959a2e3670544a56609478957352994a51c
Therese Linden <theresemlinden@gmail.com>
2023-05-17 15:47:33 +0200

    refactoring code state to controll category filtering

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -1,20 +1,40 @@
 import FilterDropDown from '../../components/FilterDropDown/FilterDropDown';
 import MovieCard from '../../components//MovieCard/MovieCard';
 import useFetch from '../../hooks/useFetch';
+import './movieList.css';
+import { useCallback, useState } from 'react';
 
 const MovieList = () => {
-  const baseUrl = `${import.meta.env.VITE_API_URL}/popular?`;
-  const api_key = `api_key=${import.meta.env.VITE_API_KEY}`;
-  const url = baseUrl + api_key;
+  //const baseUrl = `${import.meta.env.VITE_API_URL}/popular?`;
+  //const api_key = `api_key=${import.meta.env.VITE_API_KEY}`;
+  const defaultUrl = `${import.meta.env.VITE_API_URL}/popular?api_key=${
+    import.meta.env.VITE_API_KEY
+  }`;
+
+  const [url, setUrl] = useState(defaultUrl);
 
   const { data: movies, isLoading, errorMessage } = useFetch(url);
 
+  // useCallback för att ta bort eslint in FilterDropDown
+  const handleFilterChange = useCallback(category => {
+    setUrl(
+      `${import.meta.env.VITE_API_URL}/${category}?api_key=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+  }, []);
+
   return (
     <div>
-      <FilterDropDown />
+      <FilterDropDown handleFilterChange={handleFilterChange} />
       {errorMessage && <p>Something went wrong</p>}
       {isLoading && <p>Loading.....</p>}
-      {movies && <MovieCard movies={movies} />}
+      <section className="movie-grid">
+        {movies &&
+          movies.results.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+      </section>
     </div>
   );
 };
