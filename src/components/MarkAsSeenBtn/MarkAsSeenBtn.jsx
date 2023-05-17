2bd53a1ec8575b8ee95dbbca3fd0260dd34ed828
Therese Linden <theresemlinden@gmail.com>
2023-05-17 15:44:45 +0200

    using react icon instead of html button element

--- src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
+++ src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
@@ -1,5 +1,21 @@
+import { BsFillEyeFill } from 'react-icons/bs';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
 const MarkAsSeenBtn = () => {
-  return <button>Seen it</button>;
+  const handleClick = () => console.log('icon clicked');
+  //return <button>Seen it</button>;
+  return (
+    <>
+      <BsFillEyeFill
+        data-tooltip-id="my-tooltip"
+        data-tooltip-content="Mark movie as seen"
+        data-tooltip-place="bottom"
+        onClick={handleClick}
+      />
+      <Tooltip id="my-tooltip" />
+    </>
+  );
 };
 
 export default MarkAsSeenBtn;
