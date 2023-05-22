39744482a67fbc5dbf41c965982f346bcc381c56
Therese Linden <theresemlinden@gmail.com>
2023-05-22 16:31:36 +0200

    changed logic

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -3,32 +3,45 @@ import MovieCard from '../../components//MovieCard/MovieCard';
 import useFetch from '../../hooks/useFetch';
 import './movieList.css';
 import { useCallback, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const MovieList = () => {
-  //const baseUrl = `${import.meta.env.VITE_API_URL}/popular?`;
-  //const api_key = `api_key=${import.meta.env.VITE_API_KEY}`;
-  const defaultUrl = `${import.meta.env.VITE_API_URL}/popular?api_key=${
-    import.meta.env.VITE_API_KEY
-  }`;
+  const [filter, setFilter] = useSearchParams({ filter: 'popular' });
 
-  const [url, setUrl] = useState(defaultUrl);
+  const url = `${import.meta.env.VITE_API_URL}/${filter.get(
+    'filter'
+  )}?api_key=${import.meta.env.VITE_API_KEY}`;
+
+  // const defaultUrl = `${import.meta.env.VITE_API_URL}/popular?api_key=${
+  //   import.meta.env.VITE_API_KEY
+  // }`;
+
+  //const [url, setUrl] = useState(defaultUrl);
 
   const { data: movies, isLoading, errorMessage } = useFetch(url);
 
   // useCallback för att ta bort eslint in FilterDropDown
-  const handleFilterChange = useCallback(category => {
-    setUrl(
-      `${import.meta.env.VITE_API_URL}/${category}?api_key=${
-        import.meta.env.VITE_API_KEY
-      }`
-    );
-  }, []);
+  // const handleFilterChange = useCallback(category => {
+  //   setUrl(
+  //     `${import.meta.env.VITE_API_URL}/${category}?api_key=${
+  //       import.meta.env.VITE_API_KEY
+  //     }`
+  //   );
+  // }, []);
 
   return (
     <div>
-      <FilterDropDown handleFilterChange={handleFilterChange} />
+      <FilterDropDown filter={filter.get('filter')} setFilter={setFilter} />
+      {/* <FilterDropDown handleFilterChange={handleFilterChange} /> */}
       {errorMessage && <p>Something went wrong</p>}
       {isLoading && <p>Loading.....</p>}
+      <h3>
+        {filter.get('filter') === 'top_rated'
+          ? 'Top Rated'
+          : filter.get('filter')}
+        movies
+      </h3>
+
       <section className="movie-grid">
         {movies &&
           movies.results.map(movie => (
