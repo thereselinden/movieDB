e2b5c63ba22a874b284b81f3062202f6a8faae6c
Therese Linden <theresemlinden@gmail.com>
2023-05-22 12:56:49 +0200

    Wrapped with provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+import { MovieProvider } from './MovieContext';
 
 import './index.css';
 
 const App = () => {
   return (
-    <>
+    <MovieProvider>
       <Header />
       <Main />
       <Footer />
-    </>
+    </MovieProvider>
   );
 };
 
