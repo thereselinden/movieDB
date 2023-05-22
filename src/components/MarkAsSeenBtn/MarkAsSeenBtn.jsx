91980c85e9912d2ed43a4917787d63964990c8ed
Therese Linden <theresemlinden@gmail.com>
2023-05-22 12:59:41 +0200

    removed unused code

--- src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
+++ src/components/MarkAsSeenBtn/MarkAsSeenBtn.jsx
@@ -2,16 +2,14 @@ import { BsFillEyeFill } from 'react-icons/bs';
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 
-const MarkAsSeenBtn = () => {
-  const handleClick = () => console.log('icon clicked');
-  //return <button>Seen it</button>;
+const MarkAsSeenBtn = ({ handleSeenMovie }) => {
   return (
     <>
       <BsFillEyeFill
         data-tooltip-id="my-tooltip"
         data-tooltip-content="Mark movie as seen"
         data-tooltip-place="bottom"
-        onClick={handleClick}
+        onClick={() => handleSeenMovie()}
       />
       <Tooltip id="my-tooltip" />
     </>
